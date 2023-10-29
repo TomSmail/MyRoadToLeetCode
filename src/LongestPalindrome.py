63bab4613105da3dc3d9f2ece9f4f7219c09709c
TomSmail <thomas.c.smail@gmail.com>
2023-10-29 21:39:25 +0000

    Feat: Working longest palindrome

--- src/LongestPalindrome.py
+++ src/LongestPalindrome.py
@@ -1,40 +1,35 @@
 class Solution:
-    def longestPalindromHelper2(self, s: str, index: int, offset: int) -> str: 
-        if (offset != 0):
-            palindrome = s[index] + s[index]
-        else:
-            palindrome = s[index]
-        for i in range(len(s)):
-            left = (index - (i + 1)) 
-            right = (index + (i + 1) + offset)
-            print(left, right)
-            if( left < 0) or (right > len(s) - 1):
-                return palindrome
-            elif s[left] == s[right]:
-                palindrome = palindrome + s[left]
-                palindrome = s[right] + palindrome
-            else:
-                return palindrome
-        return palindrome
-
-    def longestPalindromeHelper(self, s: str, index: int) -> str:
-        palindrome = self.longestPalindromHelper2(s, index, 0)
-        palindrome2 = ""
-        if index < (len(s) - 1):
-            if s[index + 1] == palindrome:
-                palindrome2 = self.longestPalindromHelper2(s, index, 1)
-        if len(palindrome) > len(palindrome2):
-            return palindrome
-        else:
-            return palindrome2
-
     def longestPalindrome(self, s: str) -> str:
-        largest_len = 0
-        largest = ""
+        longest_palindrome_num = 0
+        longest_palindrome = ''
         for i in range(len(s)):
-            palindrome = self.longestPalindromeHelper(s, i)
-            if len(palindrome) > largest_len:
-                largest_len = len(palindrome)
-                largest = palindrome
-        return largest
+            palindrome = s[i]
+            offset = 1
+            continuous = True
+            while(continuous):
+                if i + offset < len(s):
+                    if s[i] == s[i + offset]:
+                        palindrome = palindrome + s[i + offset]
+                        offset += 1
+                    else:
+                        continuous = False
+                else:
+                    continuous = False
+            print(palindrome)
+            l = i - 1 
+            r = i + offset
+            same = True
+            palindrome_num = offset
+            while(l >= 0 and r < len(s) and same):
+                if (s[l] == s[r]):
+                    palindrome = s[l] + palindrome + s[r]
+                    palindrome_num += 2
+                    l -= 1
+                    r += 1
+                else:
+                    same = False
+            if longest_palindrome_num < palindrome_num:
+                longest_palindrome_num = palindrome_num
+                longest_palindrome = palindrome
+        return longest_palindrome
 
